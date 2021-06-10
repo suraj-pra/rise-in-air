86605fc23b6c43b45e53c62f33a86babb966216d
alokthak <thakuralok2000@gmail.com>
2021-06-10 17:15:21 +0530

    Fuel Bar Added to the game

---
+++ Assets/FuelBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public FuelCapacity fuelCapacity;
+
+    void start()
+    {
+        slider.maxValue = fuelCapacity.fuel;
+        slider.value = fuelCapacity.fuel;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    void FixedUpdate()
+    {
+        slider.value = fuelCapacity.fuel;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
