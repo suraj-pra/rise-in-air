e4c10cc3c09c9d3b0e064e908747bd25459c18c2
Katik Jhawar <kartikjh@iitk.ac.in>
2021-06-12 00:04:07 +0530

    Powerups

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject spawnee;
+    public GameObject spawnee1;
+    public GameObject spawnee2;
+    public GameObject spawnee3;
+    public GameObject spawnee4;
+    public GameObject spawnee5;
     public bool stopSpawning = false;
     public float spawnTime;
     public float spawnDelay;
@@ -16,7 +20,27 @@ public class Spawner : MonoBehaviour
 
     public void SpawnObject()
     {
-        Instantiate(spawnee, transform.position, transform.rotation);
+        int num = Random.Next(1, 6);
+        if(num == 1)
+        {
+            Instantiate(spawnee1, transform.position, transform.rotation);
+        }
+        else if(num == 2)
+        {
+            Instantiate(spawnee2, transform.position, transform.rotation);
+        }
+        else if (num == 3)
+        {
+            Instantiate(spawnee2, transform.position, transform.rotation);
+        }
+        else if (num == 4)
+        {
+            Instantiate(spawnee2, transform.position, transform.rotation);
+        }
+        else
+        {
+            Instantiate(spawnee2, transform.position, transform.rotation);
+        }
         if (stopSpawning)
         {
             CancelInvoke("SpawnObject");
