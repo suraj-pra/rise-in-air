659d6abd40a9348dd7238390cf7e96a7f52ea18b
Divyansh Garg <divyansh.gargsg@gmail.com>
2021-06-19 14:20:28 +0530

    Changes Made

--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RightMovement : MonoBehaviour
+public class LeftMovement : MonoBehaviour
 {
     public float speed;
     Vector3 movement = new Vector3(1,0,0);
-
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(speed*Time.deltaTime*movement);
