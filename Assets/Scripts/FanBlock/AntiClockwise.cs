0bab6d1adb7336155b8c1ea647d6f3ff7497be8a
Divyansh Garg <divyansh.gargsg@gmail.com>
2021-06-26 19:29:19 +0530

    Updated all the things to work the game

---
+++ Assets/Scripts/FanBlock/AntiClockwise.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiClockwise : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 1000f)* Time.deltaTime);
+        
+    }
+}
