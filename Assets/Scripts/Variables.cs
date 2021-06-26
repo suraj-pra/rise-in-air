145dbab3b3e55062e435624577ba04d2c25896eb
Divyansh Garg <divyansh.gargsg@gmail.com>
2021-06-26 18:19:24 +0530

    Added Background, MainMenu, EndMenu

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Variables : MonoBehaviour
 {
     float speed = 8f;
 
     public float getSpeed() {
         return speed;
+    }
+
+    public void EndScene(){
+        SceneManager.LoadScene("EndMenu");
     }
 }
