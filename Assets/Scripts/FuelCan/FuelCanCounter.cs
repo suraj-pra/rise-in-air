b656b1d5dcebea27d52abc6a05c20eef325500e9
suraj-pra <75700720+suraj-pra@users.noreply.github.com>
2021-06-27 13:35:29 +0530

    Changed Fuel Can Spawning
    Now, fuel can spawns after every 4-5 spwanees.

---
+++ Assets/Scripts/FuelCan/FuelCanCounter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelCanCounter : MonoBehaviour
+{
+    public int count;
+    void Start()
+    {
+        count = 0;
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject [] spawnee;
+    public GameObject fuelCan;
     public bool stopSpawning = false;
+
+    private GameObject fuelCanCounter;
+    private FuelCanCounter fuelCanCounterScript;
     
     void Start()
     {
+        fuelCanCounter = GameObject.Find("FuelCanCounter");
+        fuelCanCounterScript = fuelCanCounter.GetComponent<FuelCanCounter>();
+
         Invoke("SpawnObject", 2);
     }
 
     public void SpawnObject()
     {
-        System.Random random = new System.Random();
-        int num = random.Next(0, spawnee.Length);
-        Instantiate(spawnee[num], transform.position, transform.rotation);
+        if(fuelCanCounterScript.count >= Random.Range(4, 6))
+        {
+            Instantiate(fuelCan, new Vector3(Random.Range(-4.5f, 4.5f), 5f, transform.position.z), transform.rotation);
+            fuelCanCounterScript.count = -1;
+        }
+        else
+        {
+            System.Random random = new System.Random();
+            int num = random.Next(0, spawnee.Length);
+            Instantiate(spawnee[num], transform.position, transform.rotation);
+        }
         if (stopSpawning)
         {
             CancelInvoke("SpawnObject");
         }
+
+        fuelCanCounterScript.count++;
     }
 }
